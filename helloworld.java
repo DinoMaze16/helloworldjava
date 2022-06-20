230361d17f06b40882b84d659d8e0221d75c79ae
DinoMaze16 <107751683+DinoMaze16@users.noreply.github.com>
2022-06-20 20:13:57 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 public class helloworld {
    public static void main(String[] args) {
-    System.out.println("Hello World - This is Dinesh");
+    System.out.println("Hello World - Stranger things");
     }
 }
